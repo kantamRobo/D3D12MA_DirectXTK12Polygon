5a29ce5d6de72df4b9a6a616d7159171f42ec073
Kanta yukawa <hattendosummer@yahoo.co.jp>
2026-05-30 15:26:00 +0900

    微修正

--- D3D12MA_DirectXTK12_DXR_Assimp_Basic/Game.cpp
+++ D3D12MA_DirectXTK12_DXR_Assimp_Basic/Game.cpp
@@ -10,7 +10,7 @@ extern void ExitGame() noexcept;
 using namespace DirectX;
 
 using Microsoft::WRL::ComPtr;
-
+//
 Game::Game() noexcept(false)
 {
     m_deviceResources = std::make_unique<DX::DeviceResources>();
